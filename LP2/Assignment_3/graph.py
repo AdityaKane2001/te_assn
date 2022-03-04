1e1c3ab4a185b69ce8b640f78713bf5a020ff3a0
Aditya Kane <adityakane1@gmail.com>
2022-03-04 15:28:38 +0530

    Added prims algorithm

--- LP2/Assignment_3/graph.py
+++ LP2/Assignment_3/graph.py
@@ -1,45 +1,56 @@
-class Graph:
-    def __init__(self, num_vertices, name="my_graph"):
-        self.graph = []
-        self.num_vertices = num_vertices
-        self.name = name
+import sys # Library for INT_MAX
+ 
+class Graph():
+ 
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                    for row in range(vertices)]
     
-    def add_edge(self, start, end, weight):
-        if (start, end) in self:
-            raise ValueError(f"Edge ({start}, {end}) already in graph.")
-        elif start >= self.num_vertices or end >= self.num_vertices or start < 0 or end <= 0 or weight <=0:
-            raise ValueError(f"Please check inputs. recvd: {start}, {end}, {weight}")
-        else:
-            self.graph.append((start, end, weight))
-        return True
-    
-    def get_edge(self, start, end):
-        for edge in self.graph:
-            if edge[0] == start and edge[1] == end:
-                return edge, self.graph.index(edge)
-        print("W: Edge not found")
-        return (-1,-1,-1), -1
 
-    def remove_edge(self, start, end):
-        _, idx = self.get_edge(start, end)
-        self.graph.remove(idx)
-        return True
-
-    def get_edges_on_vertex(self, start):
-        edges = []
-        for edge in self.graph:
-            if edge[0] == start or edge[1] == start:
-                edges.append(edge)
-        return edges
-
-
-    def check_cycle(self):
-        traversed = []
-        for edge in self.graph:
-            traversed.append(edge[1])
-
-
-    def __contains__(self, key):
-        if key in self.edges:
-            return True
-        return False
+    # A utility function to print the constructed MST stored in parent[]
+    def printMST(self, parent):
+        print ("Edge \tWeight")
+        for i in range(1, self.V):
+            print (parent[i], "-", i, "\t", self.graph[i][parent[i]])
+ 
+    # A utility function to find the vertex with
+    # minimum distance value, from the set of vertices
+    # not yet included in shortest path tree
+    def minKey(self, key, mstSet):
+ 
+        # Initialize min value
+        min = sys.maxsize
+ 
+        for v in range(self.V):
+            if key[v] < min and mstSet[v] == False:
+                min = key[v]
+                min_index = v
+ 
+        return min_index
+    def primMST(self):
+        key = [sys.maxsize] * self.V
+        parent = [None] * self.V 
+        key[0] = 0
+        mstSet = [False] * self.V
+ 
+        parent[0] = -1 
+        for _ in range(self.V):
+            u = self.minKey(key, mstSet)
+            # print(u)
+            mstSet[u] = True
+            for v in range(self.V):
+               if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
+                        key[v] = self.graph[u][v]
+                        parent[v] = u
+ 
+        self.printMST(parent)
+ 
+g = Graph(5)
+g.graph =  [[0, 2, 0, 6, 0],
+            [2, 0, 3, 8, 5],
+            [0, 3, 0, 0, 7],
+            [6, 8, 0, 0, 9],
+            [0, 5, 7, 9, 0]]
+ 
+g.primMST()
