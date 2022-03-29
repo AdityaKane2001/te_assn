c03e18bcf7d14a860fd594a939956628d53d100b
pict <pict@localhost.localdomain>
2022-03-29 15:50:10 +0530

    Added insert in WTL assn 5

--- WTL/Assignment5/apache-tomcat-8.5.76/webapps/ROOT/WEB-INF/classes/Update.java
+++ WTL/Assignment5/apache-tomcat-8.5.76/webapps/ROOT/WEB-INF/classes/Update.java
@@ -1,26 +1,102 @@
-// import java.io.*;
-// import javax.servlet.*;
-// import javax.servlet.http.*;
-// import java.sql.*;
-// import java.nio.*;
-// import java.nio.file.*;
-// import java.nio.charset.*;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+import java.nio.*;
+import java.nio.file.*;
+import java.nio.charset.*;
 
-// public class Update extends HttpServlet{
-// private String mymsg;
+public class Update extends HttpServlet {
+    private String mymsg;
 
-// public void init() throws ServletException {
-// mymsg = "Http Servlet Demo";
+    public void init() throws ServletException {
+        mymsg = "Http Servlet Demo";
 
-// }
+    }
 
-// public void doGet(HttpServletRequest request, HttpServletResponse response)
-// throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            // Initialize all the information regarding
+            // Database Connection
 
-// }
+            response.setContentType("text/html");
+            // Path filepath = Path.get();
+            // String insert_html = Files.readString(path, StandardCharsets.US_ASCII);
+            // Writing the message on the web page
+            PrintWriter out = response.getWriter();
+            // out.println("<h1>" + mymsg + "</h1>");
+            // out.print(insert_html);
+            out.println("<html>  ");
+            out.println("    <body><h1> Update </h1>");
+            out.println("        <form action='/update' method='post'>");
+            out.println("            <label for='book_id'>Book id</label>");
+            out.println("            <input type='text' name='book_id' id='book_id'> <br>");
+            out.println("            <label for='book_name'>Book Name</label>");
+            out.println("            <input type='text' name='book_name' id='book_name'> <br>");
+            out.println("            <label for='author_name'>Author Name</label>");
+            out.println("            <input type='text' id='author_name' name='author_name'><br>");
+            out.println("            <label for='price'>Price</label>");
+            out.println("            <input type='text' id='price' name='price'><br>");
+            out.println("            <label for='quantity'>Quantity</label>");
+            out.println("            <input type='text' id='quantity' name='quantity'><br>");
+            out.println("            <input type='submit' value='Insert'>");
+            out.println("        </form>");
+            out.println("    </body>");
+            out.println("</html>");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-// public void doPost(HttpServletRequest request, HttpServletResponse response)
-// throws ServletException, IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            String dbDriver = "com.mysql.jdbc.Driver";
+            String dbURL = "jdbc:mysql://localhost:3306/books?characterEncoding=utf8";
+            // Database name to access
+            // String dbName = "books";
+            String dbUsername = "root";
+            String dbPassword = "Root123#";
+            Class.forName(dbDriver);
 
-// }
-// }
+            Connection conn = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+            PrintWriter out = response.getWriter();
+            PreparedStatement st = conn
+                    .prepareStatement(
+                            "update ebookshop set book_title=?, book_author=?, book_price=?, quantity=? where book_id=?;");
+
+            // For the first parameter,
+            // get the data using request object
+            // sets the data to st pointer
+            st.setString(1, request.getParameter("book_name"));
+            st.setString(2, request.getParameter("author_name"));
+            st.setInt(3, Integer.parseInt(request.getParameter("price")));
+            st.setInt(4, Integer.parseInt(request.getParameter("quantity")));
+            st.setInt(5, Integer.parseInt(request.getParameter("book_id")));
+            // Same for second parameter
+            // st.setString(2, request.getParameter("string"));
+
+            // Execute the insert command using executeUpdate()
+            // to make changes in database
+            st.executeUpdate();
+            st.close();
+            conn.close();
+
+            redirectTo("crud_home?task=select", response);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void redirectTo(String servlet, HttpServletResponse response) {
+        // Set response content type
+        response.setContentType("text/html");
+
+        // New location to be redirected
+        // String site = new String("http://www.photofuntoos.com");
+
+        response.setStatus(response.SC_MOVED_TEMPORARILY);
+        response.setHeader("Location", servlet);
+    }
+}
